57d915fe9cdcc0128765691b16625aa621184ea6
Nicolas <Nicolas@PC-Nicolas>
2015-05-20 19:13:42 +0200

    Ajout des anciennes sources.

---
+++ attribution_sujets/src/main/java/views/dialog/ModelDialog.java
@@ -0,0 +1,19 @@
+package views.dialog;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+public class ModelDialog extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ModelDialog(JFrame parent) {
+		super(parent, "Modéle");
+		
+		initializeView();
+	}
+
+	private void initializeView() {
+		
+	}
+}
