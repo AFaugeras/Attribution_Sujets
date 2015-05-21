a2626fff2d5c9718bec4b55fd18f32666a108466
kedriss <kedriss@hotmail.fr>
2015-05-21 15:02:40 +0200

    mise forme pour ludo!!

--- attribution_sujets/src/main/java/models/bean/Model.java
+++ attribution_sujets/src/main/java/models/bean/Model.java
@@ -3,34 +3,40 @@ package models.bean;
 import java.util.List;
 
 public class Model {
-private GeneralConstraints constraint;
-private List<Person> persons;
-private List<Subject> subjects;
-public Model(GeneralConstraints constraint, List<Person> persons,
-		List<Subject> subjects) {
-	super();
-	this.constraint = constraint;
-	this.persons = persons;
-	this.subjects = subjects;
-}
-public GeneralConstraints getConstraint() {
-	return constraint;
-}
-public void setConstraint(GeneralConstraints constraint) {
-	this.constraint = constraint;
-}
-public List<Person> getPersons() {
-	return persons;
-}
-public void setPersons(List<Person> persons) {
-	this.persons = persons;
-}
-public List<Subject> getSubjects() {
-	return subjects;
-}
-public void setSubjects(List<Subject> subjects) {
-	this.subjects = subjects;
-}
+	private GeneralConstraints constraint;
+	private List<Person> persons;
+	private List<Subject> subjects;
 
+	public Model(GeneralConstraints constraint, List<Person> persons,
+			List<Subject> subjects) {
+		super();
+		this.constraint = constraint;
+		this.persons = persons;
+		this.subjects = subjects;
+	}
+
+	public GeneralConstraints getConstraint() {
+		return constraint;
+	}
+
+	public void setConstraint(GeneralConstraints constraint) {
+		this.constraint = constraint;
+	}
+
+	public List<Person> getPersons() {
+		return persons;
+	}
+
+	public void setPersons(List<Person> persons) {
+		this.persons = persons;
+	}
+
+	public List<Subject> getSubjects() {
+		return subjects;
+	}
+
+	public void setSubjects(List<Subject> subjects) {
+		this.subjects = subjects;
+	}
 
 }
