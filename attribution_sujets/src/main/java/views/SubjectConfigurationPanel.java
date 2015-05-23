e3176be79763fc5c0d43e728fdfac578ae0357bc
Nicolas <Nicolas@PC-Nicolas>
2015-05-23 14:40:13 +0200

    Ajout des panel de configuration des sujets et des contraintes générales pour Arthur.

---
+++ attribution_sujets/src/main/java/views/SubjectConfigurationPanel.java
@@ -0,0 +1,15 @@
+package views;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class SubjectConfigurationPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public SubjectConfigurationPanel() {
+		super();
+		
+		add(new JLabel("Panel configuration des sujets"));
+	}
+}
