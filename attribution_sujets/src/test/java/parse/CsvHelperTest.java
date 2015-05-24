df7b84c41de7bdfa9c62cfdc409b2b5ca1dcad1c
Nicolas <Nicolas@PC-Nicolas>
2015-05-24 15:37:19 +0200

    Correction d'erreurs syntaxiques :p

--- attribution_sujets/src/test/java/parse/CsvHelperTest.java
+++ attribution_sujets/src/test/java/parse/CsvHelperTest.java
@@ -3,26 +3,26 @@ package parse;
 import java.io.File;
 
 import junit.framework.TestCase;
+import models.parser.CsvHelper;
 
 import org.junit.Test;
 
 public class CsvHelperTest extends TestCase {
 
-	
 	@Test
-	public void testOvertureFichier(){
+	public void testOvertureFichier() {
 		File f = new File("");
-		CsvHelper.getRessource(f.getAbsolutePath()+f.separator+"donnees"+f.separator+"Choix_des_sujets_2015.csv");
+		CsvHelper.getRessource(f.getAbsolutePath() + File.separator + "donnees"
+				+ File.separator + "Choix_des_sujets_2015.csv");
 	}
-	
+
 	@Test
-	public void testOvertureFichierFail(){
+	public void testOvertureFichierFail() {
 		try {
 			CsvHelper.getRessource(null);
 		} catch (NullPointerException e) {
 			System.out.println("exception conformément trouvée");
 		}
-		
-		
+
 	}
 }
--- attribution_sujets/src/test/java/parse/ParseCsvAnswerTest.java
+++ attribution_sujets/src/test/java/parse/ParseCsvAnswerTest.java
@@ -5,31 +5,36 @@ import java.io.IOException;
 import java.util.List;
 
 import junit.framework.TestCase;
+import models.factory.AnswerFactory;
 import models.interfaces.I_Answer;
+import models.parser.CsvHelper;
+import models.parser.ParserCsvAnswer;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-import factory.AnswerFactory;
-
 public class ParseCsvAnswerTest extends TestCase {
-	
-	
-@Test
-public void testSuppressionDoublon() throws IOException{
-	File f = new File("");
-	f = CsvHelper.getRessource(f.getAbsolutePath()+f.separator+"donnees"+f.separator+"Choix_avec_doublon.csv");
-	ParserCsvAnswer parser = new ParserCsvAnswer();
-	 parser.parseAnswer(f);
-	 List<I_Answer> answer = parser.getCleanedData();
-	 assertEquals(answer.get(0).getDateSoumission(), createKeptAnswerDoublon().getDateSoumission());
-	 assertEquals(answer.get(0).getIdRepondant(), createKeptAnswerDoublon().getIdRepondant());
-}
 
+	@Test
+	public void testSuppressionDoublon() throws IOException {
+		File f = new File("");
+		f = CsvHelper.getRessource(f.getAbsolutePath() + File.separator
+				+ "donnees" + File.separator + "Choix_avec_doublon.csv");
+		ParserCsvAnswer parser = new ParserCsvAnswer();
+		parser.parseAnswer(f);
+		List<I_Answer> answer = parser.getCleanedData();
+		assertEquals(answer.get(0).getDateSoumission(),
+				createKeptAnswerDoublon().getDateSoumission());
+		assertEquals(answer.get(0).getIdRepondant(), createKeptAnswerDoublon()
+				.getIdRepondant());
+	}
 
-private I_Answer createKeptAnswerDoublon(){
-	String[] data = {"28773","03/02/2015 21:38:21","A1","eleve","IPIPIP","	","4161","ADAM François","fadam14","Labyrinthes",	"Space landing","Poker",	"Bataille",	"Souris de bibliothèque",	"Ruzzle",	"Si possible, j'aimerais bien me mettre avec Maxime Ansquer."};
-	
-	return AnswerFactory.createResponse(data);
-}
+	private I_Answer createKeptAnswerDoublon() {
+		String[] data = { "28773", "03/02/2015 21:38:21", "A1", "eleve",
+				"IPIPIP", "	", "4161", "ADAM François", "fadam14",
+				"Labyrinthes", "Space landing", "Poker", "Bataille",
+				"Souris de bibliothèque", "Ruzzle",
+				"Si possible, j'aimerais bien me mettre avec Maxime Ansquer." };
+
+		return AnswerFactory.createResponse(data);
+	}
 }
