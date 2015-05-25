fe6ce19dc0fe3826c1556a80ffc59d1f5bdb5123
kedriss <kedriss@hotmail.fr>
2015-05-25 11:31:37 +0200

    Correction coquilles, test unitaires du parser de Subject, Correction de la factory Subject
    merge avec le pom de ludo

--- attribution_sujets/src/main/java/models/factory/SubjectFactory.java
+++ attribution_sujets/src/main/java/models/factory/SubjectFactory.java
@@ -1,4 +1,6 @@
 package models.factory;
+import javax.xml.crypto.Data;
+
 import models.bean.Subject;
 
 public class SubjectFactory {
@@ -13,14 +15,23 @@ public class SubjectFactory {
 	public static Subject createSubject(String[] data, int id){
 		 Subject subject= new Subject();
 		 subject.setId(id);
-		 subject.setLabel(data[0]);
-		 subject.setMaxSize(Integer.valueOf(data[1]));
-		 subject.setMinSize(Integer.valueOf(data[2]));
-		 subject.setMultiple(Integer.valueOf(data[3]));
-		 subject.setCardMin(Integer.valueOf(data[4]));
-		 subject.setCardMax(Integer.valueOf(data[5]));
+		 int i=0;
+		 subject.setLabel(data[i++]) ;// obligatoire
+		if(notNull(data, i))subject.setMaxSize(Integer.valueOf(data[i]));else subject.setMaxSize(0) ;
+		i++;
+		if(notNull(data, i)) subject.setMinSize(Integer.valueOf(data[i]));else subject.setMinSize(0);
+		i++;
+		if(notNull(data, i)) subject.setMultiple(Integer.valueOf(data[i]));else subject.setMultiple(0);
+		i++;
+		if(notNull(data, i)) subject.setCardMin(Integer.valueOf(data[i]));else subject.setCardMin(0);
+		i++;
+		if(notNull(data, i)) subject.setCardMax(Integer.valueOf(data[i]));else subject.setCardMax(0);
 		 
 		 return subject;
 		
+	}
+	
+	private static boolean notNull(String[] data, int index){
+		return data.length>index && (!data[index].isEmpty());
 	}
 }
