d96dc3121c49dac7f5c536eb9d64bc4c6b935d94
Ludo <lulu2727@hotmail.fr>
2015-05-25 10:51:30 +0200

    Creation de l'adaptateur pour Choco.

---
+++ attribution_sujets/src/main/java/models/adaptor/Adaptor.java
@@ -0,0 +1,8 @@
+package models.adaptor;
+
+/**
+ * Interface permettant d'adapter notre model de donnees (classe Model) au modele des solvers d'attribution de sujets.
+ */
+public interface Adaptor {
+
+}
