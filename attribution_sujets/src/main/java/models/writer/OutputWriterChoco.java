a26f8d3c50849e6f976c1295b1cc08f79f4f09bd
Ludo <lulu2727@hotmail.fr>
2015-05-25 10:51:59 +0200

    Creation du Writer du fichier d'entrée de Choco.

---
+++ attribution_sujets/src/main/java/models/writer/OutputWriterChoco.java
@@ -0,0 +1,61 @@
+package models.writer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import models.adaptor.AdaptorChoco;
+
+/**
+ * Module d'ecriture du fichier d'entree du Solver Choco.
+ */
+public class OutputWriterChoco{
+	
+	/**
+	 * Methode d'ecriture d'un fichier d'entree de Choco
+	 * @param filename nom du fichier de sortie
+	 * @param data model de donnees sur lequel sera base ce fichier
+	 * @throws WriterException Erreur d'ecriture
+	 */
+	public static void write(String filename, AdaptorChoco data) throws WriterException {
+		
+		try {
+			FileWriter fw = new FileWriter(filename);
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			bw.write("/* nb eleves */\n");
+			bw.write(data.getNbPersons().toString());
+			
+			bw.write("\n/* nb sujets */\n");
+			bw.write(data.getNbSubjects().toString());
+			
+			bw.write("\n/* pour chaque sujet de 1 a n, nb min de groupe puis nb max de groupes */\n");
+			bw.write(data.getSizeRange().toString());
+			
+			bw.write("\n/* effectif min et effectif max des groupes pour chaque sujet */\n");
+			bw.write(data.getCardRange().toString());
+			
+			bw.write("\n/* nb min de sujets effectivement affectés */\n");
+			bw.write(data.getMinimumAssignedSubject().toString());
+			
+			bw.write("\n/* nombre puis valeurs des pénalités (coûts des affectations) */\n");
+			bw.write(data.getRepartitionCost().toString());
+			
+			bw.write("\n/* nom prenom, nb choix, rang sujet 1.. Rang sujet n */\n");
+			bw.write(data.getChoices().toString());
+			
+			bw.write("\n/* pour chaque eleve, nb puis liste des sujets refusés */");
+			bw.write(data.getRejects().toString());
+			
+			bw.write("\n/* multiplicite des groupes */\n");
+			bw.write(data.getMultiplicity().toString());
+			
+			bw.close();
+			fw.close();
+			
+		} catch (IOException e) {
+			throw new WriterException("Erreur : Ecriture du fichier entree de choco");
+		}				
+	}
+
+}
