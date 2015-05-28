fece53c086842f955d3fbcea1f8375a667ae60af
Nicolas <Nicolas@172.17.2.145>
2015-05-28 13:32:13 +0200

    Ajout javadoc classe MainFrame.

--- attribution_sujets/src/main/java/views/MainFrame.java
+++ attribution_sujets/src/main/java/views/MainFrame.java
@@ -9,33 +9,55 @@ import javax.swing.JPanel;
 import views.configuration.ConfigurationPanel;
 import views.result.ResultPanel;
 
+/**
+ * Fenêtre principale de l'application.
+ *
+ */
 public class MainFrame extends JFrame {
 
+	// Constantes :
 	private static final long serialVersionUID = 1L;
 
 	private static final String APPLICATION_NAME = "Attribution des sujets";
 	private static final String CONFIGURATION_PANEL = "CONFIGURATION_PANEL";
 	private static final String RESULT_PANEL = "RESULT_PANEL";
-
 	private static final Dimension PREFERED_SIZE = new Dimension(1200, 650);
 
+	/**
+	 * Conteneur principal.
+	 */
 	private JPanel cardPanel;
+	
+	/**
+	 * Panel de configuration.
+	 */
 	private ConfigurationPanel configurationPanel;
+	
+	/**
+	 * Panel de résultat.
+	 */
 	private ResultPanel resultPanel;
 
+	/**
+	 * Constructeur.
+	 */
 	public MainFrame() {
 		super(APPLICATION_NAME);
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		this.initializeView();
-		// this.setResizable(false);
 
 		this.pack();
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 	}
 
+	/**
+	 * Accesseur de l'attribut configurationPanel.
+	 * 
+	 * @return Le panel configurationPanel.
+	 */
 	public ConfigurationPanel getConfigurationPanel() {
 		if (this.configurationPanel == null) {
 			this.configurationPanel = new ConfigurationPanel();
@@ -44,6 +66,12 @@ public class MainFrame extends JFrame {
 		return this.configurationPanel;
 	}
 
+
+	/**
+	 * Accesseur de l'attribut resultPanel.
+	 * 
+	 * @return Le panel resultPanel.
+	 */
 	public ResultPanel getResultPanel() {
 		if (this.resultPanel == null) {
 			this.resultPanel = new ResultPanel(null);
@@ -57,16 +85,25 @@ public class MainFrame extends JFrame {
 		return PREFERED_SIZE;
 	}
 
+	/**
+	 * Affiche le panel de configuration.
+	 */
 	public void showConfigurationPanel() {
 		((CardLayout) this.cardPanel.getLayout()).show(this.cardPanel,
 				CONFIGURATION_PANEL);
 	}
 
+	/**
+	 * Affiche le panel de résultat.
+	 */
 	public void showResultPanel() {
 		((CardLayout) this.cardPanel.getLayout()).show(this.cardPanel,
 				RESULT_PANEL);
 	}
 
+	/**
+	 * Méthode privée qui initialise la vue.
+	 */
 	private void initializeView() {
 		this.cardPanel = new JPanel(new CardLayout());
 
