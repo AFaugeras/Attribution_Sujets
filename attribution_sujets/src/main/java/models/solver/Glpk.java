79561fae29117277beab158d729922f87a5b7e25
Ludo <lulu2727@hotmail.fr>
2015-06-02 23:00:58 +0200

    Creation de la classe d'implementation du solveur Glpk

--- attribution_sujets/src/main/java/models/solver/Glpk.java
+++ attribution_sujets/src/main/java/models/solver/Glpk.java
@@ -1,8 +1,14 @@
 package models.solver;
 
+import java.io.IOException;
+
 import models.bean.Model;
+import models.solver.adaptor.AdaptorGlpk;
+import models.solver.adaptor.AdaptorGlpkImpl;
 import models.solver.reader.NotFoundSolutionException;
 import models.solver.reader.ReaderException;
+import models.solver.reader.SolutionReaderGlpk;
+import models.solver.writer.InputWriterGlpk;
 import models.solver.writer.WriterException;
 
 public class Glpk implements Solver{
@@ -10,8 +16,29 @@ public class Glpk implements Solver{
 	@Override
 	public Model solve(String inputFilename, String outputFilename, Model data)
 			throws WriterException, ReaderException, NotFoundSolutionException {
-		// TODO Auto-generated method stub
-		return null;
+
+		//TODO Supprimer cette contrainte forcee
+		data.getConstraint().getWeights().add(1000000L);
+		AdaptorGlpk ag = new AdaptorGlpkImpl(data);
+		
+		InputWriterGlpk.write(inputFilename, ag);
+			
+		String[] cmd = { "C:\\Users\\lulu2_000\\Desktop\\glpk-4.55\\w64\\glpsol", "-m", "C:\\Users\\lulu2_000\\Desktop\\intersemestre-modele.mod", "-d", inputFilename, "-w", outputFilename };
+		
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(cmd);
+			p.waitFor();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		
+		SolutionReaderGlpk.read(outputFilename, data);
+		
+		return data;
 	}
 
 }
