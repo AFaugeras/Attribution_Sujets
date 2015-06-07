7a272c68667323d818d3bb0552eb388d01cf6e1e
Ludo <lulu2727@hotmail.fr>
2015-06-07 18:27:36 +0200

    Ajout d'un message précisant l'exception

--- attribution_sujets/src/main/java/models/solver/reader/NotFoundSolutionException.java
+++ attribution_sujets/src/main/java/models/solver/reader/NotFoundSolutionException.java
@@ -10,5 +10,18 @@ public class NotFoundSolutionException extends Exception{
 	 * Version
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Message precisant l'erreur
+	 */
+	private String message;
+	
+	/**
+	 * Constructeur
+	 * @param message erreur
+	 */
+	public NotFoundSolutionException(String message){
+		this.message = message;
+	}
 
 }
