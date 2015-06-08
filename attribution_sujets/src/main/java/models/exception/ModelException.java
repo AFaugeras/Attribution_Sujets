b22e78bc88448f9a0e3c580d373df59467af7590
Ludo <lulu2727@hotmail.fr>
2015-06-08 13:52:29 +0200

    Création de ModelException en cas d'incohérence du modèle.

---
+++ attribution_sujets/src/main/java/models/exception/ModelException.java
@@ -0,0 +1,14 @@
+package models.exception;
+
+public class ModelException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ModelException(String message){
+		super(message);
+	}
+
+}
