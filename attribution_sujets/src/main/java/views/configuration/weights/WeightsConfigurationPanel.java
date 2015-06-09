26d9817ce88ff0a50a8f899b740f423d0a160243
Nicolas <Nicolas@172.17.2.145>
2015-06-09 10:40:54 +0200

    Maj javadoc. Classe : WeightsConfigurationPanel.

--- attribution_sujets/src/main/java/views/configuration/weights/WeightsConfigurationPanel.java
+++ attribution_sujets/src/main/java/views/configuration/weights/WeightsConfigurationPanel.java
@@ -19,14 +19,27 @@ public class WeightsConfigurationPanel extends JScrollPane {
 	// Constantes :
 	private static final long serialVersionUID = 1L;
 
+	private static final String LABEL_TITLE_BORDER = "Poids";
+	
+	/**
+	 * Conteneur.
+	 */
 	public JPanel jpContainer;
 
+	/**
+	 * Constructeur.
+	 */
 	public WeightsConfigurationPanel() {
 		super();
 
 		this.initializeView();
 	}
 
+	/**
+	 * Accesseur de l'attribut jpContainer.
+	 * 
+	 * @return Le panel jpContainer.
+	 */
 	public JPanel getJpContainer() {
 		if (this.jpContainer == null) {
 			this.jpContainer = new JPanel(new GridBagLayout());
@@ -35,10 +48,13 @@ public class WeightsConfigurationPanel extends JScrollPane {
 		return this.jpContainer;
 	}
 
+	/**
+	 * Cette méthode privée appelée par le constructeur initialiser la vue.
+	 */
 	private void initializeView() {
 		this.setViewportView(this.getJpContainer());
 
-		TitledBorder border = BorderFactory.createTitledBorder("Poids");
+		TitledBorder border = BorderFactory.createTitledBorder(LABEL_TITLE_BORDER);
 		Font font = border.getTitleFont();
 		border.setTitleFont(new Font(font.getName(), Font.BOLD, font.getSize()));
 		border.setBorder(new LineBorder(Color.BLACK));
