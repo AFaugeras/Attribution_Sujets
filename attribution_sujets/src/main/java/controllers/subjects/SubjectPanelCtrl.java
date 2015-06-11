b01168e1a4a4799d8535796045157ac1d108001a
Nicolas <Nicolas@PC-Nicolas>
2015-06-11 19:53:08 +0200

    Oublie d'un fichier.

--- attribution_sujets/src/main/java/controllers/subjects/SubjectPanelCtrl.java
+++ attribution_sujets/src/main/java/controllers/subjects/SubjectPanelCtrl.java
@@ -9,13 +9,25 @@ import javax.swing.event.ChangeListener;
 
 import views.configuration.subjects.SubjectPanel;
 
+/**
+ * Une instance de cette classe permet de contrôler un SubjectPanel.
+ */
 public class SubjectPanelCtrl implements ChangeListener {
 
+	// Constantes :
 	private static final Color ALERT_BACKGROUND_COLOR = new Color(238, 185, 168);
 	private static final Color DEFAULT_BACKGROUND_COLOR = UIManager.getLookAndFeel().getDefaults().getColor("EditorPane.background");
 
+	/**
+	 * La vue.
+	 */
 	private SubjectPanel view;
 
+	/**
+	 * Constructeur.
+	 * 
+	 * @param view La vue.
+	 */
 	public SubjectPanelCtrl(SubjectPanel view) {
 		super();
 
@@ -39,6 +51,12 @@ public class SubjectPanelCtrl implements ChangeListener {
 		}
 	}
 
+	/**
+	 * Vérifie que la valeur du Spinner de l'effectif min soit bien inférieure ou égale à la valeur du spinner de l'effectif max.
+	 * Si elle est supérieure on active une alerte visuelle.
+	 * 
+	 * @param jsMinSize Je Spinner de l'effectif min.
+	 */
 	private void checkMinSize(JSpinner jsMinSize) {
 		int min = (int) jsMinSize.getValue();
 		int max = (int) this.view.getJsMaxSize().getValue();
@@ -52,6 +70,11 @@ public class SubjectPanelCtrl implements ChangeListener {
 		}
 	}
 
+	/**
+	 * Vérifie que la valeur du Spinner de l'effectif max soit bien supérieure ou égale à la valeur du spinner de l'effectif min.
+	 * Si elle est inférieure on active une alerte visuelle.
+	 * @param jsMaxSize Le Spinner de l'effectif max.
+	 */
 	private void checkMaxSize(JSpinner jsMaxSize) {
 		int max = (int) jsMaxSize.getValue();
 		int min = (int) this.view.getJsMinSize().getValue();
@@ -65,6 +88,11 @@ public class SubjectPanelCtrl implements ChangeListener {
 		}
 	}
 
+	/**
+	 * Vérifie que la valeur du spinner de la cardinalité max soit bien inférieure ou égale à la valeur du spinner de la cardinalité min.
+	 * Si elle est supérieure on active une alerte visuelle.
+	 * @param jsMinCard Le Spinner de la cardinalité min.
+	 */
 	private void checkMinCard(JSpinner jsMinCard) {
 		int min = (int) jsMinCard.getValue();
 		int max = (int) this.view.getJsMaxCard().getValue();
@@ -78,6 +106,11 @@ public class SubjectPanelCtrl implements ChangeListener {
 		}
 	}
 
+	/**
+	 * Vérifie que la valeur du Spinner de la cardinalité max soit bien supérieure ou égale à la valeur du spinner de l'effectif min.
+	 * Si elle est inférieure on active une alerte visuelle.
+	 * @param jsMaxCard
+	 */
 	private void checkMaxCard(JSpinner jsMaxCard){
 		int max = (int) jsMaxCard.getValue();
 		int min = (int) this.view.getJsMinCard().getValue();
@@ -91,25 +124,28 @@ public class SubjectPanelCtrl implements ChangeListener {
 		}
 	}
 
-	public boolean isError() {
-		boolean ret = true;
-
-		int minSize = (int) this.view.getJsMinSize().getValue();
-		int maxSize = (int) this.view.getJsMaxSize().getValue();
-		int minCard = (int) this.view.getJsMinCard().getValue();
-		int maxCard = (int) this.view.getJsMaxCard().getValue();
-
-		if (minSize <= maxSize && minCard <= maxCard) {
-			ret = false;
-		}
-
-		return ret;
-	}
-
+	/**
+	 * Méthode privée appellée par le constructeur pour initialiser les réactions.
+	 */
 	private void initializeReactions() {
 		this.view.getJsMinSize().addChangeListener(this);
 		this.view.getJsMaxSize().addChangeListener(this);
 		this.view.getJsMinCard().addChangeListener(this);
 		this.view.getJsMaxCard().addChangeListener(this);
 	}
+
+//	public boolean isError() {
+//		boolean ret = true;
+//
+//		int minSize = (int) this.view.getJsMinSize().getValue();
+//		int maxSize = (int) this.view.getJsMaxSize().getValue();
+//		int minCard = (int) this.view.getJsMinCard().getValue();
+//		int maxCard = (int) this.view.getJsMaxCard().getValue();
+//
+//		if (minSize <= maxSize && minCard <= maxCard) {
+//			ret = false;
+//		}
+//
+//		return ret;
+//	}
 }
