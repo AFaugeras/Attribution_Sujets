1cef4ee0b591c32ce808e31f41cb7d7b6407b0d0
Nicolas <Nicolas@PC-Nicolas>
2015-06-11 19:55:07 +0200

    Maj javadoc. Classe : SolverSelectionPanelCtrl.

--- attribution_sujets/src/main/java/controllers/solver/SolverSelectionPanelCtrl.java
+++ attribution_sujets/src/main/java/controllers/solver/SolverSelectionPanelCtrl.java
@@ -5,14 +5,29 @@ import models.solver.Glpk;
 import models.solver.Solver;
 import views.configuration.solver.SolverSelectionPanel;
 
+/**
+ * Contrôleur du panel de sélection du solveur.
+ */
 public class SolverSelectionPanelCtrl {
 
+	/**
+	 * La vue.
+	 */
 	private SolverSelectionPanel view;
 
+	/**
+	 * Constructeur.
+	 * 
+	 * @param view La vue.
+	 */
 	public SolverSelectionPanelCtrl(SolverSelectionPanel view) {
 		this.view = view;
 	}
 
+	/**
+	 * Permet d'obtenir une instance du solveur sélectionné.
+	 * @return Un solveur.
+	 */
 	public Solver getSelectedSolver() {
 		Solver ret = null;
 		String solverName = (String) this.view.getJcbSolvers()
